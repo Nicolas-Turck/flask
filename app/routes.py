1ca30f32e91ac3528219feec9f72f7e20e5bde23
nicos <nicolas.turck@gmail.com>
2020-03-13 19:53:19 +0100

    first commit

---
+++ app/routes.py
@@ -0,0 +1,115 @@
+from app import app, db
+from flask import Flask, render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from flask_login import  current_user, login_user, logout_user, login_required
+from app.models import User
+from datetime import datetime
+
+"""i import login_required of flask login for required identification user for go to page selected """
+
+@app.before_request
+def before_request():
+    """function for display date and time of end connexion user"""
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    """function for connect to form with password and login name"""
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """function for log out user account"""
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    """function for modify user account"""
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """function for create new user account"""
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    """function for go to start page"""
+    user = {'username': 'Bresil'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/about/')
+@login_required
+def about():
+    return render_template('about.html')
+
+@app.route('/articles/')
+@login_required
+def articles():
+    return render_template('articles.html')
+
+
+
+    return app
